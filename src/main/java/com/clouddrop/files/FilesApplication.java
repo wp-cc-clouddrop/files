85f515f4eb8331a0ab455fba0fd0b9a2f58b2e15
pokgak <m.aimanismail@gmail.com>
2019-10-14 23:26:53 +0200

    initial template

---
+++ src/main/java/com/clouddrop/files/FilesApplication.java
@@ -0,0 +1,13 @@
+package com.clouddrop.files;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilesApplication.class, args);
+	}
+
+}
