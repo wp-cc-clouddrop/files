01015bd668fea9065f2d4ec1115c189c538ae874
Mohammad Fazel Soltani <mohammadfazel.soltani@haw-hamburg.de>
2019-10-22 18:56:34 +0200

    started with API implementation

---
+++ src/main/java/com/clouddrop/files/FileMetaData.java
@@ -0,0 +1,31 @@
+package com.clouddrop.files;
+
+public class FileMetaData {
+    private String filename;
+    private String type;
+    private String modDate;
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getModDate() {
+        return modDate;
+    }
+
+    public void setModDate(String modDate) {
+        this.modDate = modDate;
+    }
+}
--- src/main/java/com/clouddrop/files/FilesController.java
+++ src/main/java/com/clouddrop/files/FilesController.java
@@ -2,14 +2,9 @@ package com.clouddrop.files;
 
 import com.microsoft.azure.storage.StorageException;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.net.URISyntaxException;
 import java.security.InvalidKeyException;
@@ -17,9 +12,20 @@ import java.security.InvalidKeyException;
 @RestController
 public class FilesController {
 
+    private FilesAzureStorage fas;
+
+    public FilesController(){
+        fas = new FilesAzureStorage();
+        fas.setContainerName("guestcontainer");
+        fas.connect();
+    }
+
     @PostMapping("/files")
-    public String uploadFile() {
-        return "POST to /files";
+    public String uploadFile(
+            @RequestBody FileMetaData fileMetaData, @RequestParam("file") MultipartFile file
+            ) {
+        String a = fileMetaData.getFilename()+fileMetaData.getType()+fileMetaData.getModDate();
+        return "POST to /files"+a;
     }
 
     @PutMapping("/files")
@@ -27,9 +33,9 @@ public class FilesController {
         return "PUT to /files";
     }
 
-    @GetMapping("/files/{id}")
-    public String getFile(@PathVariable("id") Long id) {
-        return "GET to /files/" + id;
+    @GetMapping("/files/{fileName}")
+    public String getFile(@PathVariable("fileName") String fileName) {
+        return "GET to /files/" + fileName;
     }
 
     @DeleteMapping("/files/{id}")
