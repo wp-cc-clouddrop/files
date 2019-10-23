cdf1ea1ada00b18bb2d490870831eb839c813ae7
pokgak <m.aimanismail@gmail.com>
2019-10-23 15:35:22 +0200

    rewrite metadata

---
+++ src/main/java/com/clouddrop/files/model/Metadata.java
@@ -0,0 +1,59 @@
+package com.clouddrop.files.model;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Metadata {
+
+    private String filename;
+    private String type;
+    private String lastModified;
+    private String contentLocation;
+
+    public Metadata() {}
+
+    public Metadata(Metadata resource, String location) {
+        this.filename = resource.getFilename();
+        this.type = resource.getType();
+        this.contentLocation = location;
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        this.lastModified = dtf.format(now);
+    }
+
+    public String getContentLocation() {
+        return contentLocation;
+    }
+
+    public void setContentLocation(String contentLocation) {
+        this.contentLocation = contentLocation;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getLastModified() {
+        return lastModified;
+    }
+
+    public void setLastModified(String modDate) {
+        this.lastModified = modDate;
+    }
+}
