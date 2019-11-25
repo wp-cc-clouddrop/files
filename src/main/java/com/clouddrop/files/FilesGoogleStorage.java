07e18389f3782ccae8fe9982e9da6b39b231aafc
Fazel Soltani <mohammadfazel.soltani@haw-hamburg.de>
2019-11-25 17:25:35 +0100

    storage implemented but not tested

--- src/main/java/com/clouddrop/files/FilesGoogleStorage.java
+++ src/main/java/com/clouddrop/files/FilesGoogleStorage.java
@@ -1,28 +1,30 @@
 package com.clouddrop.files;
 
 import com.google.api.gax.paging.Page;
+import com.google.cloud.WriteChannel;
 import com.google.cloud.storage.*;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class FilesGoogleStorage implements IFilesAdapter {
 
     private Storage _storage;
     private Bucket _bucket;
     private String _bucketName;
-    private Blob _blob;
+    //private Blob _blob;
 
     public FilesGoogleStorage(){
         // Instantiates a client
         setStorage(StorageOptions.getDefaultInstance().getService());//ToDo we have to find a way to set here the credentials for gcp
 
         //The name of the bucket to access
-        setBucketName("guestcontainer");
+        setBucketName("guestbucket");
 
         // Creates the new bucket
         setBucket(_storage.create(BucketInfo.of(_bucketName)));
@@ -52,56 +54,59 @@ public class FilesGoogleStorage implements IFilesAdapter {
         _bucketName = bucketName;
     }
 
-    public Blob getBlob() {
-        return _blob;
-    }
-
-    public void setBlob(Blob blob) {
-        _blob = blob;
-    }
-
     @Override
     public void uploadMetadata(HashMap<String, String> metadata) {
-
-    }
-
-    public void upload(){//ToDo alles hier muss noch angepasst werden
         //https://cloud.google.com/storage/docs/uploading-objects
-        Storage storage = StorageOptions.getDefaultInstance().getService();
-        BlobId blobId = BlobId.of("bucket", "blob_name");
-        BlobInfo blobInfo = BlobInfo.newBuilder(blobId).setContentType("text/plain").build();
-        Blob blob = storage.create(blobInfo, "Hello, Cloud Storage!".getBytes());
+        BlobId blobId = BlobId.of(getBucketName(), metadata.get("username")+"-"+metadata.get("filename"));
+        BlobInfo blobInfo = BlobInfo.newBuilder(blobId).setContentType("text/plain").setMetadata(metadata).build();
+        Blob blob = getStorage().create(blobInfo, new byte[0]);
     }
 
     @Override
     public boolean updateFile(String username, String filename, byte[] data) {
-        return false;
+        //https://cloud.google.com/storage/docs/uploading-objects
+        boolean resu = true;
+
+        BlobId blobId = BlobId.of(getBucketName(), username+"-"+filename);
+
+        Blob blob = getStorage().get(blobId);
+
+        Map<String,String> metadata = blob.getMetadata();
+        BlobInfo blobInfo = BlobInfo.newBuilder(blobId).setContentType("text/plain").setMetadata(metadata).build();
+        try(WriteChannel wc = getStorage().writer(blobInfo)){
+            try{
+                wc.write(ByteBuffer.wrap(data));
+            } catch (Exception ex){
+                ex.printStackTrace();
+                resu = false;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            resu = false;
+        }
+        return resu;
     }
 
     @Override
-    public byte[] downloadFile(String username, String filename) {//ToDo username ist hier nicht relevant,weil der bucketName als Param übergeben wird
+    public byte[] downloadFile(String username, String filename) {
         // The name of the bucket to access
         String bucketName = getBucketName();
 
-        // The path to which the file should be downloaded
-        Path destFilePath = Paths.get("/local/path/to/file.txt");//ToDo hier muss das Ziel angepasst werden
-
-        // Instantiate a Google Cloud Storage client
-        Storage storage = StorageOptions.getDefaultInstance().getService();//ToDo we have to find a way to set here the credentials for gcp
-
         // Get specific file from specified bucket
-        Blob blob = storage.get(BlobId.of(bucketName, filename));
+        Blob blob = getStorage().get(BlobId.of(bucketName, username+"-"+filename));
 
         // Download file to specified path
-        blob.downloadTo(destFilePath);
-        return new byte[0];//ToDo das hier ist nicht das richtige Ergebnis
+        //blob.downloadTo(destFilePath);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        blob.downloadTo(baos,Blob.BlobSourceOption.generationMatch());
+        return baos.toByteArray();
     }
 
     @Override
-    public void deleteFile(String username, String filename) throws IllegalArgumentException {//ToDo username ist hier nicht relevant,weil der bucketName als Param übergeben wird
+    public void deleteFile(String username, String filename) throws IllegalArgumentException {
         //https://cloud.google.com/storage/docs/deleting-objects
-        BlobId blobId = BlobId.of(_bucketName, filename);
-        boolean deleted = _storage.delete(blobId);
+        BlobId blobId = BlobId.of(getBucketName(), username+"-"+filename);
+        boolean deleted = getStorage().delete(blobId);
         if (deleted) {
             // the blob was deleted
         } else {
@@ -117,13 +122,50 @@ public class FilesGoogleStorage implements IFilesAdapter {
         Page<Blob> blobs = getBucket().list();
         for (Blob blob : blobs.iterateAll()) {
             // do something with the blob
-
+            String name = blob.getName();
+            int value = name.indexOf(username);
+            if(value == -1){
+                continue;
+            }
+            //String s = String.format("\t\t%s\t: %s", ((CloudBlob) blobItem).getProperties().getBlobType(), path.substring(pathIndex+prefix.length()));
+            String s = String.format("%s", (name.substring(value+username.length())));
+            results.add(s);
         }
         return results;
     }
 
     @Override
-    public List<String> searchFile(String username, String name, String type, String date, String tag) {
-        return null;
+    public List<String> searchFile(final String username,final String name, final String typ, final String date, final String tag) {
+
+        List<String> results = new ArrayList<>();
+        Page<Blob> blobs = getBucket().list();
+        for (Blob blob : blobs.iterateAll()) {
+
+            Map<String,String> metaData = blob.getMetadata();
+
+            boolean nameExists = name != null;
+            boolean typeExists = typ != null;
+            boolean dateExists = date != null;
+            boolean tagExists = tag != null;
+            boolean matches = true;
+            if(nameExists){
+                matches &= metaData.containsValue(name);
+            }
+            if(typeExists){
+                matches &= metaData.containsValue(typ);
+            }
+            if(dateExists){
+                matches &= metaData.containsValue(date);
+            }
+            if(tagExists){
+                String[] tagsArray = metaData.get("tags").split(",");
+                HashSet<String> tagsSet = new HashSet<String>(Arrays.asList(tagsArray));
+                matches &= tagsSet.contains(tag.toLowerCase());
+            }
+            if(matches){
+                results.add(metaData.get("filename"));
+            }
+        }
+        return results;
     }
 }
